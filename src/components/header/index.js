c7ac92ff686c40b4c9e4b0d70deff1732136ac00
ozgeonec <ozgenuronec@gmail.com>
2021-09-14 23:20:47 +0300

    main test

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import * as React from 'react';
 import './styles.scss';
-// import Logo from './../../assets/graphics/logo.png';
 
 const Header = (props) => {
     return (
