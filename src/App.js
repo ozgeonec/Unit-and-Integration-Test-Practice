5be19f8622457d44f90cb288dee382d41a4bf7df
ozgeonec <ozgenuronec@gmail.com>
2021-09-14 22:51:33 +0300

    main styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import Header from "./components/header";
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+        </div>
+    );
 }
 
 export default App;
